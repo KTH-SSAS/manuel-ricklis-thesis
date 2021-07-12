8bbc597c98c437dee192e1112b8807f7286e9da1
Manuel Rickli <manuel.rickli@stud.unibas.ch>
2021-07-12 10:33:03 +0300

    Change data loading in gat training

--- value_approximator/gat/train.py
+++ value_approximator/gat/train.py
@@ -1,5 +1,7 @@
 import argparse
+import json
 import time
+from glob import glob
 
 from sklearn.metrics import f1_score
 from sklearn.metrics import r2_score
@@ -12,6 +14,7 @@ from helpers.constants import *
 from value_approximator.gat.gat import GAT
 from helpers.gat_utils import *
 
+
 # TODO: since we work with multiple graphs, implement the data loader à la PPI code
 #       each data loader returns train, val & test sets with (features, labels, edge_index) respectively
 
@@ -22,8 +25,12 @@ def train_gat(config):
     # I've added the option to force the use of CPU even though you have a GPU on your system (but it's too weak).
     device = torch.device("cuda" if torch.cuda.is_available() and not config['force_cpu'] else "cpu")
 
-    # Step 1: prepare the data loaders
-    data_loader_train, data_loader_val, data_loader_test = load_graph_data(config, device)
+    # Step 1: get available graph data sets
+    graph_data_sets = [
+        glob(f'AttackGraphs/train_[0-9]*.json'),
+        glob(f'AttackGraphs/val_[0-9]*.json'),
+        glob(f'AttackGraphs/test_[0-9]*.json')
+    ]
 
     # Step 2: prepare the model
     gat = GAT(
@@ -42,24 +49,26 @@ def train_gat(config):
 
     # The decorator function makes things cleaner since there is a lot of redundancy between the train and val loops
     main_loop = get_main_loop(
-        config,
-        gat,
-        loss_fn,
-        optimizer,
-        config['patience_period'],
-        time.time())
+        graph_data_sets=graph_data_sets,
+        config=config,
+        gat=gat,
+        loss_function=loss_fn,
+        optimizer=optimizer,
+        patience_period=config['patience_period'],
+        time_start=time.time()
+    )
 
     BEST_VAL_PERF, BEST_VAL_LOSS, PATIENCE_CNT = [0, 0, 0]  # reset vars used for early stopping
 
     # Step 4: Start the training procedure
     for epoch in range(config['num_of_epochs']):
         # Training loop
-        main_loop(phase=LoopPhase.TRAIN, data_loader=data_loader_train, epoch=epoch)
+        main_loop(phase=LoopPhase.TRAIN, epoch=epoch)
 
         # Validation loop
         with torch.no_grad():
             try:
-                main_loop(phase=LoopPhase.VAL, data_loader=data_loader_val, epoch=epoch)
+                main_loop(phase=LoopPhase.VAL, epoch=epoch)
             except Exception as e:  # "patience has run out" exception :O
                 print(str(e))
                 break  # break out from the training loop
@@ -68,7 +77,7 @@ def train_gat(config):
     # Don't overfit to the test dataset - only when you've fine-tuned your model on the validation dataset should you
     # report your final loss and micro-F1 on the test dataset. Friends don't let friends overfit to the test data. <3
     if config['should_test']:
-        micro_f1 = main_loop(phase=LoopPhase.TEST, data_loader=data_loader_test)
+        micro_f1 = main_loop(phase=LoopPhase.TEST)
         config['test_perf'] = micro_f1
 
         print('*' * 50)
@@ -84,103 +93,104 @@ def train_gat(config):
 
 
 # Simple decorator function so that I don't have to pass arguments that don't change from epoch to epoch
-def get_main_loop(config, gat, loss_function, optimizer, node_features, node_labels, edge_index, train_indices,
-                  val_indices, test_indices, patience_period, time_start):
-    node_dim = 0  # node axis
-
-    train_labels = node_labels.index_select(node_dim, train_indices)
-    val_labels = node_labels.index_select(node_dim, val_indices)
-    test_labels = node_labels.index_select(node_dim, test_indices)
-
+def get_main_loop(graph_data_sets, config, gat, loss_function, optimizer, patience_period, time_start):
     # node_features shape = (N, FIN), edge_index shape = (2, E)
-    graph_data = (node_features, edge_index)  # I pack data into tuples because gat uses nn.Sequential which requires it
+    # graph_data = (node_features, edge_index)  # I pack data into tuples because gat uses nn.Sequential which requires it
 
-    def get_node_indices(phase):
-        if phase == LoopPhase.TRAIN:
-            return train_indices
-        elif phase == LoopPhase.VAL:
-            return val_indices
-        else:
-            return test_indices
+    def get_graph_data(file_names: []):
+        node_features = []
+        node_labels = []
+        edge_index = []
+        for file_name in file_names:
+            with open(file_name, "r") as f:
+                graph = AttackGraph(import_dict=json.load(f))
+                _node_features, _adjacency_list = parse_features(graph, config['number_of_features'],
+                                                                config['embedding_vector_lengths'])
+                _node_labels, _ = graph.value_iteration()
+                _topology = build_edge_index(_adjacency_list, len(_node_labels), False)
 
-    def get_node_labels(phase):
-        if phase == LoopPhase.TRAIN:
-            return train_labels
-        elif phase == LoopPhase.VAL:
-            return val_labels
-        else:
-            return test_labels
+                node_features.append(_node_features.clone().detach())
+                node_labels.append(torch.tensor(_node_labels, dtype=torch.long, device=config['device']))
+                edge_index.append(torch.tensor(_topology, dtype=torch.long, device=config['device']))
+
+        return torch.cat(node_features, 0), torch.cat(node_labels, 0), torch.cat(edge_index, 1)
 
     def main_loop(phase, epoch=0):
         global BEST_VAL_PERF, BEST_VAL_LOSS, PATIENCE_CNT, writer
+        graph_data_set = []
 
         # Certain modules behave differently depending on whether we're training the model or not.
         # e.g. nn.Dropout - we only want to drop model weights during the training.
         if phase == LoopPhase.TRAIN:
             gat.train()
+            graph_data_set = graph_data_sets[0]
+        elif phase == LoopPhase.VAL:
+            gat.eval()
+            graph_data_set = graph_data_sets[1]
         else:
             gat.eval()
+            graph_data_set = graph_data_sets[2]
 
-        node_indices = get_node_indices(phase)
-        gt_node_labels = get_node_labels(phase)  # gt stands for ground truth
+        for i in range(0, len(graph_data_set) - 1, 2):
+            node_features, gt_node_labels, edge_index = get_graph_data(graph_data_set[i:i + 1])
+            graph_data = (node_features, edge_index)
 
-        # Do a forwards pass and extract only the relevant node scores (train/val or test ones)
-        # Note: [0] just extracts the node_features part of the data (index 1 contains the edge_index)
-        # shape = (N, C) where N is the number of nodes in the split (train/val/test) and C is the number of classes
-        nodes_unnormalized_scores = gat(graph_data)[0].index_select(node_dim, node_indices)
+            # Do a forwards pass and extract only the relevant node scores (train/val or test ones)
+            # Note: [0] just extracts the node_features part of the data (index 1 contains the edge_index)
+            # shape = (N, C) where N is the number of nodes in the split (train/val/test) and C is the number of classes
+            nodes_unnormalized_scores = gat(graph_data)[0]
 
-        loss = loss_function(nodes_unnormalized_scores, gt_node_labels)
+            loss = loss_function(nodes_unnormalized_scores, gt_node_labels)
 
-        if phase == LoopPhase.TRAIN:
-            optimizer.zero_grad()  # clean the trainable weights gradients in the computational graph (.grad fields)
-            loss.backward()  # compute the gradients for every trainable weight in the computational graph
-            optimizer.step()  # apply the gradients to weights
+            if phase == LoopPhase.TRAIN:
+                optimizer.zero_grad()  # clean the trainable weights gradients in the computational graph (.grad fields)
+                loss.backward()  # compute the gradients for every trainable weight in the computational graph
+                optimizer.step()  # apply the gradients to weights
 
-        # Calculate the main metric - accuracy
-        accuracy = r2_score(nodes_unnormalized_scores, gt_node_labels)
+            # Calculate the main metric - accuracy
+            accuracy = r2_score(nodes_unnormalized_scores, gt_node_labels)
 
+            #
+            # Logging
+            #
 
-        #
-        # Logging
-        #
+            if phase == LoopPhase.TRAIN:
+                # Log metrics
+                if config['enable_tensorboard']:
+                    writer.add_scalar('training_loss', loss.item(), epoch)
+                    writer.add_scalar('training_acc', accuracy, epoch)
 
-        if phase == LoopPhase.TRAIN:
-            # Log metrics
-            if config['enable_tensorboard']:
-                writer.add_scalar('training_loss', loss.item(), epoch)
-                writer.add_scalar('training_acc', accuracy, epoch)
+                # Save model checkpoint
+                if config['checkpoint_freq'] is not None and (epoch + 1) % config['checkpoint_freq'] == 0:
+                    ckpt_model_name = f'gat_{config["dataset_name"]}_ckpt_epoch_{epoch + 1}.pth'
+                    config['test_perf'] = -1
+                    torch.save(get_training_state(config, gat), os.path.join(CHECKPOINTS_PATH, ckpt_model_name))
 
-            # Save model checkpoint
-            if config['checkpoint_freq'] is not None and (epoch + 1) % config['checkpoint_freq'] == 0:
-                ckpt_model_name = f'gat_{config["dataset_name"]}_ckpt_epoch_{epoch + 1}.pth'
-                config['test_perf'] = -1
-                torch.save(get_training_state(config, gat), os.path.join(CHECKPOINTS_PATH, ckpt_model_name))
+            elif phase == LoopPhase.VAL:
+                # Log metrics
+                if config['enable_tensorboard']:
+                    writer.add_scalar('val_loss', loss.item(), epoch)
+                    writer.add_scalar('val_acc', accuracy, epoch)
 
-        elif phase == LoopPhase.VAL:
-            # Log metrics
-            if config['enable_tensorboard']:
-                writer.add_scalar('val_loss', loss.item(), epoch)
-                writer.add_scalar('val_acc', accuracy, epoch)
+                # Log to console
+                if config['console_log_freq'] is not None and epoch % config['console_log_freq'] == 0:
+                    print(
+                        f'GAT training: time elapsed= {(time.time() - time_start):.2f} [s] | epoch={epoch + 1} | val acc={accuracy}')
 
-            # Log to console
-            if config['console_log_freq'] is not None and epoch % config['console_log_freq'] == 0:
-                print(
-                    f'GAT training: time elapsed= {(time.time() - time_start):.2f} [s] | epoch={epoch + 1} | val acc={accuracy}')
+                # The "patience" logic - should we break out from the training loop? If either validation acc keeps going up
+                # or the val loss keeps going down we won't stop
+                if accuracy > BEST_VAL_PERF or loss.item() < BEST_VAL_LOSS:
+                    BEST_VAL_PERF = max(accuracy, BEST_VAL_PERF)  # keep track of the best validation accuracy so far
+                    BEST_VAL_LOSS = min(loss.item(), BEST_VAL_LOSS)  # and the minimal loss
+                    PATIENCE_CNT = 0  # reset the counter every time we encounter new best accuracy
+                else:
+                    PATIENCE_CNT += 1  # otherwise keep counting
 
-            # The "patience" logic - should we break out from the training loop? If either validation acc keeps going up
-            # or the val loss keeps going down we won't stop
-            if accuracy > BEST_VAL_PERF or loss.item() < BEST_VAL_LOSS:
-                BEST_VAL_PERF = max(accuracy, BEST_VAL_PERF)  # keep track of the best validation accuracy so far
-                BEST_VAL_LOSS = min(loss.item(), BEST_VAL_LOSS)  # and the minimal loss
-                PATIENCE_CNT = 0  # reset the counter every time we encounter new best accuracy
+                if PATIENCE_CNT >= patience_period:
+                    raise Exception('Stopping the training, the universe has no more patience for this training.')
+
             else:
-                PATIENCE_CNT += 1  # otherwise keep counting
-
-            if PATIENCE_CNT >= patience_period:
-                raise Exception('Stopping the training, the universe has no more patience for this training.')
-
-        else:
-            return accuracy  # in the case of test phase we just report back the test accuracy
+                return accuracy  # in the case of test phase we just report back the test accuracy
 
     return main_loop  # return the decorated function
 
